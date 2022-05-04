3208b0161faa799715f20268ce9b6c98e3854171
Lucas Eduardo Campolino <campolino.l@gmail.com>
2022-05-04 20:55:17 -0300

    Usuário cadastra um galpão

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -2,4 +2,18 @@ class WarehousesController < ApplicationController
     def show
         @warehouse = Warehouse.find(params[:id])
     end
+
+    def new
+    end
+
+    def create
+        # 1 - Receber os dados enviados
+
+        # 2 - Criar um novo galpão no banco de dados
+        warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :description, :address, :cep, :area)
+        w = Warehouse.new(warehouse_params) # Strong parameters
+        w.save
+
+        redirect_to root_path
+    end
 end
