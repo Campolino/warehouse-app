b7ed41ba95053e2ea77c0b07aefd336d0189a437
Lucas Eduardo Campolino <campolino.l@gmail.com>
2022-05-08 02:10:45 -0300

    Corrigidos erros de teste e acrescentado o teste para nome de galpão unico

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,5 +1,6 @@
 class Warehouse < ApplicationRecord
     validates :name, :code, :city, :description, :address, :cep, :area, presence: true
     validates :code, uniqueness: true
+    validates :name, uniqueness: true
     validates :cep, format: { with: /\d{5}-?\d{3}/ } 
 end
--- spec/models/warehouse_spec.rb
+++ spec/models/warehouse_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Warehouse, type: :model do
         context "presence" do
             it 'falso quando name está vazio' do
                 # Arrange
-                warehouse = Warehouse.new(name: '', code: 'RIO', address: 'Rua da avenida, 550', cep: '25000-000', area: 10_000, description: 'Aquele galpão')
+                warehouse = Warehouse.new(name: '', code: 'RIO', city: 'Rio de Janero', address: 'Rua da avenida, 550', cep: '25000-000', area: 10_000, description: 'Aquele galpão')
                 # Act
                 result = warehouse.valid?
                 # Assert
@@ -14,7 +14,16 @@ RSpec.describe Warehouse, type: :model do
     
             it 'falso quando code está vazio' do
                 # Arrange
-                warehouse = Warehouse.new(name: 'Galpão do Rio', code: '', address: 'Rua da avenida, 550', cep: '25000-000', area: 10_000, description: 'Aquele galpão')
+                warehouse = Warehouse.new(name: 'Galpão do Rio', code: '', city: 'Rio de Janero', address: 'Rua da avenida, 550', cep: '25000-000', area: 10_000, description: 'Aquele galpão')
+                # Act
+                result = warehouse.valid?
+                # Assert
+                expect(result).to eq false 
+            end
+
+            it 'falso quando city está vazio' do
+                # Arrange
+                warehouse = Warehouse.new(name: 'Galpão do Rio', code: 'RIO', city: '', address: 'Rua da avenida, 550', cep: '25000-000', area: 10_000, description: 'Aquele galpão')
                 # Act
                 result = warehouse.valid?
                 # Assert
@@ -23,7 +32,7 @@ RSpec.describe Warehouse, type: :model do
     
             it 'falso quando address está vazio' do
                 # Arrange
-                warehouse = Warehouse.new(name: 'Galpão do Rio', code: 'RIO', address: '', cep: '25000-000', area: 10_000, description: 'Aquele galpão')
+                warehouse = Warehouse.new(name: 'Galpão do Rio', code: 'RIO', city: 'Rio de Janero', address: '', cep: '25000-000', area: 10_000, description: 'Aquele galpão')
                 # Act
                 result = warehouse.valid?
                 # Assert
@@ -32,7 +41,7 @@ RSpec.describe Warehouse, type: :model do
     
             it 'falso quando cep está vazio' do
                 # Arrange
-                warehouse = Warehouse.new(name: 'Galpão do Rio', code: 'RIO', address: 'Rua da avenida, 550', cep: '', area: 10_000, description: 'Aquele galpão')
+                warehouse = Warehouse.new(name: 'Galpão do Rio', code: 'RIO', city: 'Rio de Janero', address: 'Rua da avenida, 550', cep: '', area: 10_000, description: 'Aquele galpão')
                 # Act
                 result = warehouse.valid?
                 # Assert
@@ -41,7 +50,7 @@ RSpec.describe Warehouse, type: :model do
     
             it 'falso quando area está vazio' do
                 # Arrange
-                warehouse = Warehouse.new(name: 'Galpão do Rio', code: 'RIO', address: 'Rua da avenida, 550', cep: '25000-000', area: nil, description: 'Aquele galpão')
+                warehouse = Warehouse.new(name: 'Galpão do Rio', code: 'RIO', city: 'Rio de Janero', address: 'Rua da avenida, 550', cep: '25000-000', area: nil, description: 'Aquele galpão')
                 # Act
                 result = warehouse.valid?
                 # Assert
@@ -50,7 +59,7 @@ RSpec.describe Warehouse, type: :model do
     
             it 'falso quando description está vazio' do
                 # Arrange
-                warehouse = Warehouse.new(name: 'Galpão do Rio', code: 'RIO', address: 'Rua da avenida, 550', cep: '25000-000', area: 10_000, description: '')
+                warehouse = Warehouse.new(name: 'Galpão do Rio', code: 'RIO', city: 'Rio de Janero', address: 'Rua da avenida, 550', cep: '25000-000', area: 10_000, description: '')
                 # Act
                 result = warehouse.valid?
                 # Assert
@@ -58,10 +67,21 @@ RSpec.describe Warehouse, type: :model do
             end
         end
         
-        it 'falso quanto code já foi usado' do
+        it 'falso quando code já foi usado' do
             # Arrange
-            first_warehouse = Warehouse.create(name: 'Galpão do Rio', code: 'RIO', address: 'Rua da avenida, 550', cep: '25000-000', area: 25_000, description: 'Aquele galpão')
-            second_warehouse = Warehouse.new(name: 'Galpão de Niteroi', code: 'RIO', address: 'Rua da lapa, 860', cep: '28000-000', area: 16_250, description: 'Outro galpão')
+            first_warehouse = Warehouse.create(name: 'Galpão do Rio', code: 'RIO', city: 'Rio de Janero', address: 'Rua da avenida, 550', cep: '25000-000', area: 25_000, description: 'Aquele galpão')
+            second_warehouse = Warehouse.new(name: 'Galpão de Niteroi', code: 'RIO', city: 'Niterói', address: 'Rua da lapa, 860', cep: '28000-000', area: 16_250, description: 'Outro galpão')
+
+            # Act
+            result = second_warehouse.valid?
+            # Assert
+            expect(result).to eq false
+        end
+
+        it 'falso quando nome já foi usado' do
+            # Arrange
+            first_warehouse = Warehouse.create(name: 'Galpão do Rio', code: 'RIO', city: 'Rio de Janero', address: 'Rua da avenida, 550', cep: '25000-000', area: 25_000, description: 'Aquele galpão')
+            second_warehouse = Warehouse.new(name: 'Galpão do Rio', code: 'NET', city: 'Niterói', address: 'Rua da lapa, 860', cep: '28000-000', area: 16_250, description: 'Outro galpão')
 
             # Act
             result = second_warehouse.valid?
