20a913462335af4e5541e6d78ea65ea62179c4e8
Lucas Eduardo Campolino <campolino.l@gmail.com>
2022-05-08 22:08:02 -0300

    Repetições removidas

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -1,17 +1,14 @@
 class WarehousesController < ApplicationController
+    before_action :set_warehouse, only: [:edit, :update, :show, :destory]
+
     def show
-        @warehouse = Warehouse.find(params[:id])
     end
 
     def new
         @warehouse = Warehouse.new
     end
 
-    def create
-        # 1 - Receber os dados enviados
-        # 2 - Criar um novo galpão no banco de dados
-        warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :description, :address, :cep, :area)
-        
+    def create        
         @warehouse = Warehouse.new(warehouse_params) # Strong parameters
         
         if @warehouse.save
@@ -24,21 +21,25 @@ class WarehousesController < ApplicationController
     end
 
     def edit
-        id = params[:id]
-        @warehouse = Warehouse.find(id)
     end
     
     def update
-        id = params[:id]
-        @warehouse = Warehouse.find(id)
-
-        warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :description, :address, :cep, :area)
-        
         if @warehouse.update(warehouse_params)
             redirect_to warehouse_path(@warehouse.id), notice: 'Galpão atualizado com sucesso'
         else
             flash.now[:notice] = 'Não foi possível atualizar o galpão'
             render 'edit'
         end
+    end
+
+    private
+
+    def set_warehouse
+        id = params[:id]
+        @warehouse = Warehouse.find(id)
+    end
+
+    def warehouse_params
+        params.require(:warehouse).permit(:name, :code, :city, :description, :address, :cep, :area)
     end
 end
