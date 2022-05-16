e63a6a40ca9c2c949b2211fe07ea812c010ce12c
Lucas Eduardo Campolino <campolino.l@gmail.com>
2022-05-16 12:33:03 -0300

    Implementado autenticação de usuário

--- spec/system/user_sign_in_spec.rb
+++ spec/system/user_sign_in_spec.rb
@@ -21,8 +21,33 @@ describe 'Usuário se autentica' do
         expect(page).to have_content 'Login efetuado com sucesso.'
         within('nav') do
             expect(page).not_to have_link 'Login' 
-            expect(page).to have_link 'Sair'
+            expect(page).to have_button 'Sair'
             expect(page).to have_content 'lucas@email.com'
         end
+    end
+
+    it 'e faz logout' do
+        # Arrange
+        User.create!(email: 'lucas@email.com', password: 'password')
+
+        # Act
+        visit root_path
+
+        click_on 'Login'
+
+        within('form') do
+            fill_in 'E-mail', with: 'lucas@email.com'
+            fill_in 'Senha', with: 'password'
+
+            click_on 'Entrar'
+        end
+
+        click_on 'Sair'
+
+        # Assert
+        expect(page).to have_content 'Logout efetuado com sucesso.'
+        expect(page).to have_link 'Login' 
+        expect(page).not_to have_button 'Sair'
+        expect(page).not_to have_content 'lucas@email.com'
     end
 end
---
+++ spec/system/user_sign_up_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe 'Usuário se autentica' do
+    it 'com sucesso' do
+        # Arrange
+
+        # Act
+        visit root_path
+
+        click_on 'Login'
+        click_on 'Criar conta'
+
+        within('form') do
+            fill_in 'Nome', with: 'Jose'
+            fill_in 'E-mail', with: 'jose@email.com'
+            fill_in 'Senha', with: '123456'
+            fill_in 'Confirme sua senha', with: '123456'
+            click_on 'Criar conta'
+        end
+
+        # Assert
+        expect(page).to have_content 'jose@email.com'
+        expect(page).to have_content 'Boas vindas! Você realizou seu registro com sucesso.'
+        expect(page).to have_button 'Sair'
+
+        user = User.last
+        expect(user.name).to eq 'Jose'
+    end
+end
