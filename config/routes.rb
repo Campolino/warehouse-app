b46b6f20956fc14c9e3fd5785c180255e2807ae5
Lucas Eduardo Campolino <campolino.l@gmail.com>
2022-06-07 15:03:41 -0300

    Adicionado API do show Warehouse

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,10 @@ Rails.application.routes.draw do
   resources :orders, only: [:index, :new, :create, :show, :edit, :update] do
     get 'search', on: :collection
   end
+
+  namespace :api do
+    namespace :v1 do
+      resources :warehouses, only: [:show]
+    end
+  end
 end
